f658c362af0c058ea23f00e4b2c408b3104edc35
gemaramadhan08 <gmrmdhn08@gmail.com>
2024-05-16 16:14:19 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,4 @@
+const loginMenu = document.querySelector("#loginMenu");
+loginMenu.addEventListener("click", function () {
+  loginMenu.classList.toggle("hidden");
+});
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#153448",
+        abu: "#3C5B6F",
+        secondary: "#948979",
+        krem: "#DFD0B8",
+      },
+    },
+  },
+  plugins: [],
+};
